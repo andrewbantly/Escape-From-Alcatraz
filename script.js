6e7c04173f9d7abd9b58e72b0db51419b44da7b5
andrewbantly <andrewbantly@gmail.com>
2023-03-19 16:40:25 -0700

    added a home page, some style, new objects and functionality

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let gameOn = null;
+let gameOn = true;
 // DOM SELECTORS
-const gameStatus = document.querySelector("#gameStatus");
+const startButton = document.querySelector("#startButton")
 const gameDistance = document.querySelector("#distance");
 const gameCanvas = document.querySelector("#canvas");
 const gameReset = document.querySelector("#gameReset");
+const gameStatus = document.querySelector("#gameStatus");
+const gameStatusHeader = document.querySelector("#gameStatusHeader");
+
+// ONLOAD SETUP
+function onLoad () {
+gameDistance.innerText = `${gameCanvas.width - (fugative.x + freeLand.width + 50)}`;
+clearInterval(pageLoad)}
+let pageLoad = setInterval(onLoad, 1);
 
 // CANVAS SETUP
 const ctx = gameCanvas.getContext("2d");
@@ -20,6 +28,7 @@ class EscapeGame {
         this.color = color;
         this.hero = hero;
         this.atLarge = true;
+        this.aFreePerson = false;
         // ADD obstacle.type object literal to define obstacles characteristics? 
     }
     // COLLISION DETECTION FUNCTION FOR ALL OBSTACLES
@@ -32,11 +41,17 @@ class EscapeGame {
                 console.log(`The fugative was caught by the ${this.color} police.`)
                 fugative.atLarge = false;
                 clearInterval(gameLoopInterval);
-                return true;
-            } else {
-                return false;
             }
             }
+        // WIN DETECTION
+        detectWin() {
+            const left = this.x <= fugative.width + fugative.x; 
+            if (left) {
+                fugative.aFreePerson = true;
+                fugative.atLarge = true;
+                clearInterval(gameLoopInterval);
+            }
+        }
             // OBSTACLE MOVEMENT 
                // 
             obstacleMovement () {
@@ -65,21 +80,22 @@ class EscapeGame {
 const canvasWidth = (getComputedStyle(canvas).width);
 const canvasHeight = (getComputedStyle(canvas).height);
 // Use math to ensure starting points of objects are relative to screen size
-const fugative = new EscapeGame(10, (gameCanvas.height / 2), 50, 20, "beige", true);
+const fugative = new EscapeGame(100, (gameCanvas.height / 2), 50, 20, "beige", true);
+const alcatraz = new EscapeGame(0, 0, 100, (gameCanvas.height), "black", false);
+const freeLand = new EscapeGame ((gameCanvas.width - 100), 0, 100, (gameCanvas.height), "yellow", false)
 const police1 = new EscapeGame((gameCanvas.width / 4), (gameCanvas.height / 3), 75, 100, "blue", false);
 const police2 = new EscapeGame((gameCanvas.width / 2), (gameCanvas.height - (gameCanvas.height / 4)), 75, 100, "orange", false);
 const police3 = new EscapeGame((gameCanvas.width - (gameCanvas.width / 5)), (gameCanvas.height / 5), 75, 100, "red", false);
 
-// RENDER REFRESH
-const gameLoopInterval = setInterval (gameLoop, 50);
 
 // OBSTACLE MOVEMENT FUNCTION
 function fugativeSearch () {
-police1.obstacleMovement();
-police2.obstacleMovement();
-police3.obstacleMovement();
+    police1.obstacleMovement();
+    police2.obstacleMovement();
+    police3.obstacleMovement();
 }
-// Loop for obstacle movement
+// RENDER REFRESH
+const gameLoopInterval = setInterval (gameLoop, 50);
 let searchLoop = setInterval(fugativeSearch, 1000);
 
 // HUMAN KEYBOARD CONTROLS 
@@ -110,8 +126,8 @@ function keyPressEvent(e) {
                      fugative.x += distance;
                 } break;
             }
-    gameStatus.innerText = `distance remaing: ${(canvas).width - fugative.x}`;
-    console.log(`fugative x: ${fugative.x} y: ${fugative.y}`)
+    gameDistance.innerText = `${gameCanvas.width - (fugative.x + freeLand.width + 50)}`;
+    // console.log(`fugative x: ${fugative.x} y: ${fugative.y}`)
     } 
     // gameOn === false code here
 }
@@ -129,13 +145,27 @@ function gameLoop () {
     police1.detectHit();
     police2.detectHit();
     police3.detectHit();
-    if (fugative.atLarge === true) {
+    freeLand.detectWin();
+    if (fugative.atLarge === true && fugative.aFreePerson === false) {
         fugative.render();
+        alcatraz.render();
+        freeLand.render();
         police1.render();
         police2.render();
         police3.render();
+    } else if (fugative.atLarge === true && fugative.aFreePerson === true) {
+        freeLand.render();
+        alcatraz.render();
+        fugative.render();
+        gameStatusHeader.innerText = "You got away!"
+        gameDistance.innerText = "Escape again?";
+        document.removeEventListener("keydown", keyPressEvent);
+        winner();
     } else {
-        gameStatus.innerText = "You've been caught by the police";
+        freeLand.render();
+        alcatraz.render();
+        gameStatusHeader.innerText = "You've been caught by the police!";
+        gameDistance.innerText = "Try to escape again?";
         document.removeEventListener("keydown", keyPressEvent);
         console.log("Code here to switch to game reset");
     } 
@@ -143,3 +173,7 @@ function gameLoop () {
     }
 }   
 
+function winner () {
+    console.log("Player wins");
+
+}
