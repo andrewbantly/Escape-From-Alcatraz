4f38f109f7a9a3293bef2d8ca444bf4722a5dc12
andrewbantly <andrewbantly@gmail.com>
2023-03-20 22:53:45 -0700

    added boat imagery to canvas

--- script.js
+++ script.js
@@ -33,6 +33,36 @@ fugativeImage.onload=function() {
 }
 fugativeImage.src = "./media/fugative.png";
 
+const policeBoat1 = new Image ();
+let policeBoat1X = (gameCanvas.width / 4);
+let policeBoat1Y = (gameCanvas.height / 3);
+let policeBoat1Width = 100;
+let policeBoat1Height = 75;
+policeBoat1.onload=function () {
+    ctx.drawImage(policeBoat1, policeBoat1X, policeBoat1Y, policeBoat1Width, policeBoat1Height);
+}
+policeBoat1.src = "./media/police-boat1.png"
+
+const policeBoat2 = new Image ();
+let policeBoat2X = (gameCanvas.width / 2);
+let policeBoat2Y = (gameCanvas.height - (gameCanvas.height / 4));
+let policeBoat2Width = 100;
+let policeBoat2Height = 75;
+policeBoat2.onload=function () {
+    ctx.drawImage(policeBoat2, policeBoat2X, policeBoat2Y, policeBoat2Width, policeBoat2Height);
+}
+policeBoat2.src = "./media/police-boat2.png"
+
+const policeBoat3 = new Image ();
+let policeBoat3X = (gameCanvas.width - (gameCanvas.width / 4));
+let policeBoat3Y = (gameCanvas.height / 5);
+let policeBoat3Width = 75;
+let policeBoat3Height = 100;
+policeBoat3.onload=function () {
+    ctx.drawImage(policeBoat3, policeBoat3X, policeBoat3Y, policeBoat3Width, policeBoat3Height);
+}
+policeBoat3.src = "./media/police-boat3.png"
+
 // GAME CLASS
 class EscapeGame {
     constructor(x, y, width, height, color, hero) {
@@ -71,18 +101,60 @@ class EscapeGame {
         }
             // OBSTACLE MOVEMENT 
                // 
-            obstacleMovement () {
+            obstacleMovement1 () {
                 let i = 0;
                 const obstacleDistance = 10;
                 let randomDirection = Math.floor(Math.random() * 4);
                 if (randomDirection === 0 && this.x > (gameCanvas.height / 2)) {  
                         this.x -= obstacleDistance;
+                        policeBoat1X -= obstacleDistance;
                     } else if (randomDirection === 1  && this.x <= (gameCanvas.width - (gameCanvas.height / 2))- this.width) {
                         this.x += obstacleDistance;
+                        policeBoat1X += obstacleDistance;
                     } else if (randomDirection === 2 && this.y >= this.height) {
                         this.y -= obstacleDistance;
+                        policeBoat1Y -= obstacleDistance;
                     } else if (randomDirection === 3 && this.y <= gameCanvas.height - this.height) {
                         this.y += obstacleDistance;
+                        policeBoat1Y += obstacleDistance;
+                    } else {
+                    }
+                };
+            obstacleMovement2 () {
+                let i = 0;
+                const obstacleDistance = 10;
+                let randomDirection = Math.floor(Math.random() * 4);
+                if (randomDirection === 0 && this.x > (gameCanvas.height / 2)) {  
+                        this.x -= obstacleDistance;
+                        policeBoat2X -= obstacleDistance;
+                    } else if (randomDirection === 1  && this.x <= (gameCanvas.width - (gameCanvas.height / 2))- this.width) {
+                        this.x += obstacleDistance;
+                        policeBoat2X += obstacleDistance;
+                    } else if (randomDirection === 2 && this.y >= this.height) {
+                        this.y -= obstacleDistance;
+                        policeBoat2Y -= obstacleDistance;
+                    } else if (randomDirection === 3 && this.y <= gameCanvas.height - this.height) {
+                        this.y += obstacleDistance;
+                        policeBoat2Y += obstacleDistance;
+                    } else {
+                    }
+                };
+            obstacleMovement3 () {
+                let i = 0;
+                const obstacleDistance = 10;
+                let randomDirection = Math.floor(Math.random() * 4);
+                if (randomDirection === 0 && this.x > (gameCanvas.height / 2)) {  
+                        this.x -= obstacleDistance;
+                        policeBoat3X -= obstacleDistance;
+                    } else if (randomDirection === 1  && this.x <= (gameCanvas.width - (gameCanvas.height / 2))- this.width) {
+                        this.x += obstacleDistance;
+                        policeBoat3X += obstacleDistance;
+                    } else if (randomDirection === 2 && this.y >= this.height) {
+                        this.y -= obstacleDistance;
+                        policeBoat3Y -= obstacleDistance;
+                    } else if (randomDirection === 3 && this.y <= gameCanvas.height - this.height) {
+                        this.y += obstacleDistance;
+                        policeBoat3Y += obstacleDistance;
                     } else {
                     }
                 };
@@ -100,15 +172,15 @@ const canvasWidth = (getComputedStyle(canvas).width);
 const canvasHeight = (getComputedStyle(canvas).height);
 let fugative = new EscapeGame((gameCanvas.width / (13/2)), (gameCanvas.height / 2), 56, 23, "rgba(0, 0, 0, 0)", true);
 let freeLand = new EscapeGame ((gameCanvas.width - 100), 0, 100, (gameCanvas.height), "yellow", false)
-let police1 = new EscapeGame((gameCanvas.width / 4), (gameCanvas.height / 3), 75, 100, "blue", false);
-let police2 = new EscapeGame((gameCanvas.width / 2), (gameCanvas.height - (gameCanvas.height / 4)), 75, 100, "orange", false);
-let police3 = new EscapeGame((gameCanvas.width - (gameCanvas.width / 5)), (gameCanvas.height / 5), 75, 100, "red", false);
+let police1 = new EscapeGame((gameCanvas.width / 4), (gameCanvas.height / 3), 100, 75, "rgba(0, 0, 0, 0)", false);
+let police2 = new EscapeGame((gameCanvas.width / 2), (gameCanvas.height - (gameCanvas.height / 4)), 100, 75, "rgba(0, 0, 0, 0)", false);
+let police3 = new EscapeGame((gameCanvas.width - (gameCanvas.width / 4)), (gameCanvas.height / 5), 75, 100, "rgba(0, 0, 0, 0)", false);
 
 // OBSTACLE MOVEMENT FUNCTION
 function fugativeSearch () {
-    police1.obstacleMovement();
-    police2.obstacleMovement();
-    police3.obstacleMovement();
+    police1.obstacleMovement1();
+    police2.obstacleMovement2();
+    police3.obstacleMovement3();
 }
 // RENDER REFRESH
 let gameLoopInterval = setInterval (gameLoop, 50);
@@ -117,7 +189,7 @@ let searchLoop = setInterval(fugativeSearch, 1000);
 // HUMAN KEYBOARD CONTROLS 
 function keyPressEvent(e) {
     if (gameOn === true) {
-    const distance = 4;
+    const distance = 2;
     switch(e.key) {
         case "w":
             case "ArrowUp":
@@ -178,6 +250,9 @@ function gameLoop () {
     if (gameOn === true) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);  
     ctx.drawImage(fugativeImage, fugativeImageX, fugativeImageY);
+    ctx.drawImage(policeBoat1, policeBoat1X, policeBoat1Y, policeBoat1Width, policeBoat1Height);
+    ctx.drawImage(policeBoat2, policeBoat2X, policeBoat2Y, policeBoat2Width, policeBoat2Height);
+    ctx.drawImage(policeBoat3, policeBoat3X, policeBoat3Y, policeBoat3Width, policeBoat3Height);
     police1.detectHit();
     police2.detectHit();
     police3.detectHit();
@@ -225,14 +300,22 @@ function gameResetFunction () {
     fugative.aFreePerson = false;
     fugativeImageX = (gameCanvas.width / (13/2));
     fugativeImageY = (gameCanvas.height / 2);
+    policeBoat1X = (gameCanvas.width / 4);
+    policeBoat1Y = (gameCanvas.height / 3);
+    policeBoat2X = (gameCanvas.width / 2);
+    policeBoat2Y = (gameCanvas.height - (gameCanvas.height / 4));
+    policeBoat3X = (gameCanvas.width - (gameCanvas.width / 4));
+    policeBoat3Y = (gameCanvas.height / 5);
     fugative = new EscapeGame((gameCanvas.width / (13/2)), (gameCanvas.height / 2), 56, 23, "rgba(0, 0, 0, 0)", true);
     freeLand = new EscapeGame ((gameCanvas.width - 100), 0, 100, (gameCanvas.height), "yellow", false)
-    police1 = new EscapeGame((gameCanvas.width / 4), (gameCanvas.height / 3), 75, 100, "blue", false);
-    police2 = new EscapeGame((gameCanvas.width / 2), (gameCanvas.height - (gameCanvas.height / 4)), 75, 100, "orange", false);
-    police3 = new EscapeGame((gameCanvas.width - (gameCanvas.width / 5)), (gameCanvas.height / 5), 75, 100, "red", false);
+    police1 = new EscapeGame((gameCanvas.width / 4), (gameCanvas.height / 3), 100, 75, "rgba(0, 0, 0, 0)", false);
+    police2 = new EscapeGame((gameCanvas.width / 2), (gameCanvas.height - (gameCanvas.height / 4)), 100, 75, "rgba(0, 0, 0, 0)", false);
+    police3 = new EscapeGame((gameCanvas.width - (gameCanvas.width / 4)), (gameCanvas.height / 5), 75, 100, "rgba(0, 0, 0, 0)", false);
+    clearInterval(gameLoopInterval);
+    clearInterval(searchLoop);
     gameLoopInterval = setInterval (gameLoop, 50);
     searchLoop = setInterval(fugativeSearch, 1000);
     document.addEventListener("keydown", keyPressEvent);
-    gameDistance.innerText = `${gameCanvas.width - (fugative.x + freeLand.width + 50)}`;
+    gameDistance.innerText = `${(gameCanvas.width - (fugative.x + freeLand.width + 50).toFixed(0))}`;
     gameStatusHeader.innerText = "Distance remaining:"
 }
